a38705749c0259d6d361c59e981c56ee1b7d3ef6
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2023-12-27 04:38:05 +0530

    Pagination Completed

--- app/src/main/java/com/devrachit/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/devrachit/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -81,8 +81,9 @@ class BreakingNewsFragment:Fragment(R.layout.fragment_breaking_news){
             val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
             val isAtLastItem= firstVisibleItemPosition+visibleItemCount >=totalItemCount
             val isNotAtBeginning= firstVisibleItemPosition>=0
-            val isTotalMoreThanVisit =totalItemCount >= QUERY_PAGE_SIZE
-            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisit && isScrolling
+            val isTotalMoreThanVisible =totalItemCount >= QUERY_PAGE_SIZE
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning &&
+                    isTotalMoreThanVisible && isScrolling
              if(shouldPaginate)
              {
                  viewModel.getBreakingNews("in")
